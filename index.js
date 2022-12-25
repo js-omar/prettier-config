92b5c41316dcba8bb927b3b9ff40c5b7bb9a5664
Omar Elsayed <omar.alsayed@syscodeia.ae>
2022-12-25 05:15:14 +0200

    fix: publish package to npm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     { files: ["**/*.{html,scss}"], options: { printWidth: 500 } },
     { files: ["**/*.html"], options: { parser: "html" } },
     {
-      files: ["**/*.modal.html", "**/*.page.html", "**/*.component.html"],
+      files: ["**/*.{modal,component,page}.html"],
       options: { parser: "angular" },
     },
   ],
